9b75ffdfa9bdee351e104801d19472be3775014c
Olenka <pulya660@gmail.com>
2024-06-21 21:02:31 +0300

    Clone solution HilelTests.sln

---
+++ AtataUITests/PageObjects/DemoQAButtonsPage.cs
@@ -0,0 +1,32 @@
+﻿using _ = AtataUITests.PageObjects.DemoQAButtonsPage;
+
+namespace AtataUITests.PageObjects
+{
+    [Url("/buttons")]
+    public sealed class DemoQAButtonsPage : DemoQAPage<_>
+    {
+        [FindById("doubleClickBtn")]
+        public Button<_> DoubleClickMe { get; private set; }
+
+        [FindById("rightClickBtn")]
+        public Button<_> RigthClickMe { get; private set; }
+
+        [ScrollTo]
+        [WaitSeconds(5, TriggerEvents.BeforeClick)]
+        public Button<_> ClickMe { get; private set; }
+
+        [FindById("dynamicClickMessage")]
+        public Text<_> DinamicClickMessage { get; private set; }
+
+        [FindById("rightClickMessage")]
+        public Text<_> RightClickMessage { get; private set; }
+
+        [FindById("doubleClickMessage")]
+        public Text<_> DoubleClickMessage { get; private set; }
+
+        [FindByClass("text-center")]
+        public Text<_> H1TitleButtons { get; private set; }
+
+            
+    }
+}
---
+++ AtataUITests/PageObjects/DemoQACheckBoxPage.cs
@@ -0,0 +1,17 @@
+﻿using Atata;
+using AtataUITests.PageObjects.Controls;
+using _ = AtataUITests.PageObjects.DemoQACheckBoxPage;
+
+namespace AtataUITests.PageObjects
+{
+    [Url("/checkbox")]
+    public sealed class DemoQACheckBoxPage : DemoQAPage<_>
+    {
+        public CheckBoxTree<_> CheckBoxTree { get; set; }
+
+        //[FindByClass("text-success")]
+        [FindByClass("display-result")]
+        public Text<_> DisplayResult { get; set; }
+
+    }
+}
